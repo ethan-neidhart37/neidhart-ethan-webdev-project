57e526c224a6e2593880e9860838b2f447df784b
Ethan Neidhart <neidhart.e@husky.neu.edu>
2017-04-26 23:47:06 -0400

    project folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/project'));
 
 //require ("./assignment/app.js")(app);
 require ("./project/app.js")(app);
